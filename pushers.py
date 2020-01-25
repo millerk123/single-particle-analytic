7391d37c903a24acd1ef8146b8ab837a4de7d841
Kyle Miller <kglenmiller@gmail.com>
2020-01-24 16:21:08 -0800

    Adding all other pushers.
    Something is wrong with the Euler pusher though...

--- pushers.py
+++ pushers.py
@@ -1,9 +1,10 @@
 import numpy as np
-dt = 0.014
+dt = 0.14
 rqm = -1.0
 a0 = 5.0
 omega0 = 1.0
 phi0 = np.pi/2
+# Available pushers: boris, vay, cary, fullrot, euler
 
 def main():
 
@@ -24,7 +25,7 @@ def main():
 
     # Analytic correction to initial momentum from Boris pusher
     p[1,:] = a0*dt/2 - np.sqrt( ( np.sqrt( np.square( a0*dt*p[0,:] )
-                                  + np.square( 1 + np.square( p[0,:] ) ) )
+                                  + np.square( 1 + np.square(p[0,:]) ) )
                                   - 1 - np.square(p[0,:]) ) / 2 )
 
     t_final = 300.0
@@ -36,7 +37,7 @@ def main():
 
     diag_x[0,:,0] = x
     diag_p[:,:,0] = p
-    dudt = dudt_boris
+    dudt = dudt_euler
 
     for n in np.arange(n_steps):
 
@@ -44,7 +45,8 @@ def main():
     
     diag_gamma = np.sqrt( np.sum( np.square(diag_p), axis=0) )
 
-    np.savez( 'single-part', x=diag_x, p=diag_p, gamma=diag_gamma, dt=dt, n_steps=n_steps )
+    np.savez( 'single-part-'+dudt.__name__[5:], x=diag_x, p=diag_p, gamma=diag_gamma,
+                dt=dt, n_steps=n_steps )
 
 # Assumes all arrays are of shape [dim,part]
 # Except x, which is shape [part] since we don't need to keep track of other 2 dimensions
@@ -72,9 +74,7 @@ def dudt_boris( p, ep, bp, n ):
     ep = ep * tem
     utemp = p + ep
 
-    u2 = np.sum( np.square(utemp), axis=0 )
-    gamma = np.sqrt( u2 + 1.0 )
-    gam_tem = tem / gamma
+    gam_tem = tem / np.sqrt( 1.0 + np.sum( np.square(utemp), axis=0 ) )
 
     bp = bp * gam_tem
 
@@ -85,6 +85,124 @@ def dudt_boris( p, ep, bp, n ):
     utemp = utemp + np.cross(p,bp,axis=0)
 
     p = utemp + ep
+
+    return p
+
+def dudt_vay( p, ep, bp, n ):
+
+    tem = 0.5 * dt / rqm
+
+    ep = ep * tem
+    bp = bp * tem
+    rgamma = 1.0 / np.sqrt( 1.0 + np.sum( np.square(p), axis=0 ) )
+
+    temp_vec = p * rgamma
+    bpsq = np.sum( np.square(bp), axis=0 )
+
+    utemp = p + ep + np.cross(temp_vec,bp,axis=0)
+
+    temp_vec = utemp + ep
+
+    ustar = np.sum( temp_vec*bp, axis=0 )
+    sigma = 1.0 + np.sum( np.square(temp_vec), axis=0 ) - bpsq
+
+    rgam = 1.0 / np.sqrt( 0.5 * ( sigma + np.sqrt( np.square(sigma)
+                      + 4.0 * ( bpsq + np.square(ustar) ) ) ) )
+
+    bp = bp * rgam
+
+    spar = 1.0 / ( 1.0 + np.sum( np.square(bp), axis=0 ) )
+    uptp = np.sum( temp_vec*bp, axis=0 )
+
+    p = spar * ( temp_vec + uptp * bp + np.cross(temp_vec,bp,axis=0) )
+
+    return p
+
+def dudt_cary( p, ep, bp, n ):
+
+    tem = 0.5 * dt / rqm
+
+    ep = ep * tem
+    bp = bp * tem
+    utemp = p + ep
+
+    gam_minus_sq = 1.0 + np.sum( np.square(utemp), axis=0 )
+    bpsq = np.sum( np.square(bp), axis=0 )
+    bdotusq = np.square( np.sum( bp*utemp, axis=0 ) )
+    gamma = np.sqrt( 0.5 * ( gam_minus_sq - bpsq
+        + np.sqrt( np.square( gam_minus_sq - bpsq ) + 4.0 *( bpsq + bdotusq ) ) ) )
+
+    bp = bp / gamma
+
+    p = utemp + np.cross(utemp,bp,axis=0)
+
+    bp = bp * 2.0 / ( 1.0 + np.sum( np.square(bp), axis=0 ) )
+
+    utemp = utemp + np.cross(p,bp,axis=0)
+
+    p = utemp + ep
+
+    return p
+
+def dudt_fullrot( p, ep, bp, n ):
+
+    tem = 0.5 * dt / rqm
+
+    ep = ep * tem
+    utemp = p + ep
+
+    tem_gam = tem / np.sqrt( 1.0 + np.sum( np.square(utemp), axis=0 ) )
+
+    bp = bp * tem_gam
+
+    bnorm = np.sqrt( np.sum( np.square(bp), axis=0 ) )
+    t = np.ones_like( bnorm )
+    inds = bnorm>0
+    t[inds] = np.tan( bnorm[inds] ) / bnorm[inds]
+    bp = bp * t
+
+    p = utemp + np.cross(utemp,bp,axis=0)
+
+    bp = bp * 2.0 / ( 1.0 + np.sum( np.square(bp), axis=0 ) )
+
+    utemp = utemp + np.cross(p,bp,axis=0)
+
+    p = utemp + ep
+
+    return p
+
+def dudt_euler( p, ep, bp, n ):
+
+    tem = 0.5 * dt / rqm
+
+    ep = ep * tem
+    utemp = p + ep
+
+    gam_tem = dt / ( rqm * np.sqrt( 1.0 + np.sum( np.square(utemp), axis=0 ) ) )
+
+    bp = bp * gam_tem
+
+    bnorm = np.sqrt( np.sum( np.square(bp), axis=0 ) )
+
+    s = np.sin( bnorm / 2.0 )
+    a = np.cos( bnorm / 2.0 )
+    inds = bnorm>0
+    s[inds] = - s[inds] / bnorm[inds]
+    s[~inds] = -1
+
+    b = bp[0,:] * s
+    c = bp[1,:] * s
+    d = bp[2,:] * s
+
+    r11 = a*a+b*b-c*c-d*d;  r21=2*(b*c-a*d);      r31=2*(b*d+a*c)
+    r12 = 2*(b*c+a*d);      r22=a*a+c*c-b*b-d*d;  r32=2*(c*d-a*b)
+    r13 = 2*(b*d-a*c);      r23=2*(c*d+a*b);      r33=a*a+d*d-b*b-c*c
+
+    p[0,:] = r11 * utemp[0,:] + r21 * utemp[1,:] + r31 * utemp[2,:]
+    p[1,:] = r12 * utemp[0,:] + r22 * utemp[1,:] + r32 * utemp[2,:]
+    p[2,:] = r13 * utemp[0,:] + r23 * utemp[1,:] + r33 * utemp[2,:]
+
+    p = p + ep
 
     return p
 
